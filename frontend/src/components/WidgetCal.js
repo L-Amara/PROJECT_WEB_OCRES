194fc0c752bbeecb0dc1e088b04836b6197b399a
RemiNem <playeremi764@gmail.com>
2021-12-06 17:59:46 +0100

    Widget YT fixed

---
+++ frontend/src/components/WidgetCal.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import axios from 'axios';
+import { oauth2 } from 'googleapis/build/src/apis/oauth2';
+import { OAuth2Client } from 'google-auth-library';
+//import http2 from 'googleapis-common/build/src/http2.js';
+const { google } = require('googleapis');
+
+const { OAuth2 } = google.auth;
+
+const oAUth2Client = new OAuth2('183168940637-1cn3rd91gde932dor17j5glqeipjmuc3.apps.googleusercontent.com',
+  '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')
+
+oAUth2Client.setCredentials({
+  refresh_token:
+    '1//04PQ265kcVd1zCgYIARAAGAQSNwF-L9IrtKgYDDqfACl9N8iZ4sAtyGl6I8CRmd8SyzZqJhYZFi4w6U8j2Qmk-WzqBnKqmBP1e5M'
+})
+
+class WidgetCal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  addMeeting(newmeeting) {
+    const calendar = google.calendar({ version: 'v3', auth: oAUth2Client })
+
+    const eventStartTime = new Date()
+    eventStartTime.setDate(eventStartTime.getDate() + 2)      //event start tomorrow
+
+    const eventEndTime = new Date()
+    eventEndTime.setDate(eventEndTime.getDate() + 2)     //end on the same day, but 45min later
+    eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
+
+    console.log('Start ', eventStartTime)
+    console.log('End ', eventEndTime)
+
+
+    const event = {
+      summary: 'Meeting test',
+      location: '37 Quai de Grenelle, 75015 Paris',
+      colorId: 1,
+      description: 'super test about new event so hyped :)',
+      start: {
+        dateTime: eventStartTime,
+        timeZone: 'Europe/Paris',
+      },
+      end: {
+        dateTime: eventEndTime,
+        timeZone: 'Europe/Paris',
+      },
+
+    }
+
+    calendar.freebusy.query(
+      {
+        resource: {
+          timeMin: eventStartTime,
+          timeMax: eventEndTime,
+          timeZone: 'Europe/Paris',
+          items: [{ id: 'primary' }],
+        },
+      },
+      (err, res) => {
+        if (err) return console.error('Freebusy query error: ', err)
+
+        const eventsArray = res.data.calendars.primary.busy
+
+        if (eventsArray.length === 0)
+          return calendar.events.insert(
+            { calendarId: 'primary', resource: event },
+            err => {
+              if (err) return console.error('Calendar event creation error: ', err)
+
+              return console.log('Calendar event created')
+            })
+        return console.log('Cannot add, busy')
+      })
+
+  }
+
+  render() {
+    return (
+      <div class="col-lg-6 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Hoverable Table</h4>
+            <p class="card-description">
+              Add class <code>.table-hover</code>
+            </p>
+            <div class="table-responsive">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>User</th>
+                    <th>Product</th>
+                    <th>Sale</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Jacob</td>
+                    <td>Photoshop</td>
+                    <td class="text-danger"> 28.76% <i class="ti-arrow-down"></i></td>
+                    <td><label class="badge badge-danger">Pending</label></td>
+                  </tr>
+                  <tr>
+                    <td>Messsy</td>
+                    <td>Flash</td>
+                    <td class="text-danger"> 21.06% <i class="ti-arrow-down"></i></td>
+                    <td><label class="badge badge-warning">In progress</label></td>
+                  </tr>
+                  <tr>
+                    <td>John</td>
+                    <td>Premier</td>
+                    <td class="text-danger"> 35.00% <i class="ti-arrow-down"></i></td>
+                    <td><label class="badge badge-info">Fixed</label></td>
+                  </tr>
+                  <tr>
+                    <td>Peter</td>
+                    <td>After effects</td>
+                    <td class="text-success"> 82.00% <i class="ti-arrow-up"></i></td>
+                    <td><label class="badge badge-success">Completed</label></td>
+                  </tr>
+                  <tr>
+                    <td>Dave</td>
+                    <td>53275535</td>
+                    <td class="text-success"> 98.05% <i class="ti-arrow-up"></i></td>
+                    <td><label class="badge badge-warning">In progress</label></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WidgetCal;
--- frontend/src/components/widgetYT.js
+++ frontend/src/components/widgetYT.js
@@ -22,117 +22,98 @@ class WidgetYT extends React.Component {
   }
 
   getall() {
-    let etat = undefined;
-    axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&key=${API_KEY}`)
+
+    axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&fields=items(snippet%2Ftitle)&key=${API_KEY}`)
       .then(res => {
-        const nvinfo = res.data;
-        console.log(nvinfo.pageInfo.totalResults);
-        etat = nvinfo.pageInfo.totalResults;
+        const nvtitle = res.data;
+        Object.entries(nvtitle).length !== 0 ? this.setState({ title: nvtitle.items[0].snippet.title }) : this.setState({ title: 'No match, you need the channel username, not the title' })
       });
-      console.log("etat "+ etat);
-      if(etat === 0){
-        this.setState({title : "Sorry, wrong channel title"});
-        this.setState({subscriberCount : "0"});
-        this.setState({viewCount : "0"});
-        this.setState({videoCount : "0"});
-        
-      }
-      else{
 
-        axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&fields=items(snippet%2Ftitle)&key=${API_KEY}`)
-    .then(res => {
-      const nvtitle = res.data;
-      //console.log(nvtitle.items[0].snippet.title);
-      this.setState({ title: nvtitle.items[0].snippet.title });
-    });
-  
-  axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=${this.state.recherche}&fields=items(statistics%2FsubscriberCount)&key=${API_KEY}`)
-    .then(res => {
-      const nvsubscriberCount = res.data;
-      //console.log(nvsubscriberCount);
-      this.setState({ subscriberCount: nvsubscriberCount.items[0].statistics.subscriberCount });    //
-    });
-  axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=${this.state.recherche}&fields=items(statistics%2FviewCount)&key=${API_KEY}`)
-    .then(res => {
-      const nvviewCount = res.data;
-      //console.log(nvviewCount);
-      this.setState({ viewCount: nvviewCount.items[0].statistics.viewCount });    //
-    })
+    axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=${this.state.recherche}&fields=items(statistics%2FsubscriberCount)&key=${API_KEY}`)
+      .then(res => {
+        const nvsubscriberCount = res.data;
+        //console.log(nvsubscriberCount);
+        Object.entries(nvsubscriberCount).length !== 0 ? this.setState({ subscriberCount: nvsubscriberCount.items[0].statistics.subscriberCount }) : this.setState({ subscriberCount: ' ' })    //
+      });
+    axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=${this.state.recherche}&fields=items(statistics%2FviewCount)&key=${API_KEY}`)
+      .then(res => {
+        const nvviewCount = res.data;
+        //console.log(nvviewCount);
+        Object.entries(nvviewCount).length !== 0 ? this.setState({ viewCount: nvviewCount.items[0].statistics.viewCount }) : this.setState({ viewCount: ' ' })   //
+      })
     axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=${this.state.recherche}&fields=items(statistics%2FvideoCount)&key=${API_KEY}`)
-    .then(res => {
-      const nvvideoCount = res.data;
-      //console.log(nvvideoCount);
-      this.setState({ videoCount: nvvideoCount.items[0].statistics.videoCount });    //
-    })
-  axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&fields=items(id)&key=${API_KEY}`)
-    .then(res => {
-      const id = res.data;
-      this.setState({ id });
-    })
-  axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&fields=items(snippet%2Fthumbnails%2Fdefault%2Furl)&key=${API_KEY}`)
-    .then(res => {
-      const nvPPurl = res.data;       //.items.snippet.thumbnails.high.url
-      //console.log(nvPPurl.items[0].snippet.thumbnails.default.url);
-      this.setState({ ppurl: nvPPurl.items[0].snippet.thumbnails.default.url });
-    })
+      .then(res => {
+        const nvvideoCount = res.data;
+        Object.entries(nvvideoCount).length !== 0 ? this.setState({ videoCount: nvvideoCount.items[0].statistics.videoCount }) : this.setState({ videoCount: ' ' })    //
+      })
+    axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&fields=items(id)&key=${API_KEY}`)
+      .then(res => {
+        const id = res.data;
+        Object.entries(id).length !==0  ? this.setState({ id }) : this.setState({ id: ' ' })
+      })
+    axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${this.state.recherche}&fields=items(snippet%2Fthumbnails%2Fdefault%2Furl)&key=${API_KEY}`)
+      .then(res => {
+        const nvPPurl = res.data;       //.items.snippet.thumbnails.high.url
+        //console.log(nvPPurl.items[0].snippet.thumbnails.default.url);
+        Object.entries(nvPPurl).length !==0  ? this.setState({ ppurl: nvPPurl.items[0].snippet.thumbnails.default.url }) : this.setState({ ppurl: 'https://wtmatter.com/wp-content/uploads/2019/10/YouTube-Unique-Channel-URL.png' })
+      })
     console.log(this.state.title);
-      }
-      
   }
 
-  componentDidMount()
-  {
-    this.getall();
-  }
 
-  render() {
-    return (
-      <div className="container">
+componentDidMount()
+{
+  this.getall();
+}
+
+render() {
+  return (
+    <div className="container">
 
 
-        <div className="card">
-          <div className="card-body">
-            <p className="card-title">youtube Details</p>
-            <p className="font-weight-500">Type the name of the youtube channel</p>
+      <div className="card">
+        <div className="card-body">
+          <p className="card-title">Youtube Details</p>
+          <p className="font-weight-500">Type the name of the youtube channel</p>
 
-            <div className="input-group">
-              <div className="input-group-prepend hover-cursor" id="navbar-search-icon">
-                <span className="input-group-text" id="search">
-                  <i className="icon-search"  onClick={() => this.getall()} ></i>
-                </span>
-              </div>
-              <input type="text" className="form-control" id="navbar-search-input" placeholder="DrakeOfficial" aria-label="search" aria-describedby="search" onChange={(e) => this.termeRecherche(e)} onKeyPress={(e) => { if (e.key === 'Enter') this.getall() }}/>
+          <div className="input-group">
+            <div className="input-group-prepend hover-cursor" id="navbar-search-icon">
+              <span className="input-group-text" id="search">
+                <i className="icon-search" onClick={() => this.getall()} ></i>
+              </span>
             </div>
-            <div className ="row mt-3 ml-1 d-flex align-items-center">
-            <img className="photoProfil mr-3" src={this.state.ppurl}></img>
-            <h1 className ="text-primary">{this.state.title}</h1>
-
-            </div>
-            
-            <div className="d-flex flex-wrap mb-5">
-              <div className="mr-5 mt-3">
-                <p className="text-muted">Subscribers </p>
-                <h3 className="text-primary fs-30 font-weight-medium">{this.state.subscriberCount}</h3>
-              </div>
-              <div className="mr-5 mt-3">
-                <p className="text-muted">Views</p>
-                <h3 className="text-primary fs-30 font-weight-medium">{this.state.viewCount}</h3>
-              </div>
-              <div className="mr-5 mt-3">
-                <p className="text-muted">Videos</p>
-                <h3 className="text-primary fs-30 font-weight-medium">{this.state.videoCount}</h3>
-              </div>
-
-            </div>
-            
-            <canvas id="order-chart"></canvas>
+            <input type="text" className="form-control" id="navbar-search-input" placeholder="DrakeOfficial" aria-label="search" aria-describedby="search" onChange={(e) => this.termeRecherche(e)} onKeyPress={(e) => { if (e.key === 'Enter') this.getall() }} />
           </div>
+          <div className="row mt-3 ml-1 d-flex align-items-center">
+            <img className="photoProfil mr-3" src={this.state.ppurl}></img>
+            <h1 className="text-primary">{this.state.title}</h1>
+
+          </div>
+
+          <div className="d-flex flex-wrap mb-5">
+            <div className="mr-5 mt-3">
+              <p className="text-muted">Subscribers </p>
+              <h3 className="text-primary fs-30 font-weight-medium">{this.state.subscriberCount}</h3>
+            </div>
+            <div className="mr-5 mt-3">
+              <p className="text-muted">Views</p>
+              <h3 className="text-primary fs-30 font-weight-medium">{this.state.viewCount}</h3>
+            </div>
+            <div className="mr-5 mt-3">
+              <p className="text-muted">Videos</p>
+              <h3 className="text-primary fs-30 font-weight-medium">{this.state.videoCount}</h3>
+            </div>
+
+          </div>
+
+          <canvas id="order-chart"></canvas>
         </div>
       </div>
+    </div>
 
 
-    );
-  }
+  );
+}
 }
 
 export default WidgetYT;
