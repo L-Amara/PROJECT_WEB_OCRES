eb6a35fec7dde80db8bb6f8721d61ca7e229d33c
RemiNem <playeremi764@gmail.com>
2021-12-08 21:17:32 +0100

    calendar showing

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -3,6 +3,7 @@ import WidgetYT from './components/widgetYT';
 import Footer from './components/footer';
 import Spotify from './Spotify';
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
+import WidgetCal3 from './components/widgetCal3';
 
 
 class Dashboard extends React.Component{
@@ -553,31 +554,9 @@ class Dashboard extends React.Component{
                     
                     <div className="row">
                         <div className="col-md-6 grid-margin stretch-card">
-                        <div className="card">
-                            <div className="card-body">
-                            <p className="card-title">Order Details</p>
-                            <p className="font-weight-500">The total number of sessions within the date range. It is the period time a user is actively engaged with your website, page or app, etc</p>
-                            <div className="d-flex flex-wrap mb-5">
-                                <div className="mr-5 mt-3">
-                                <p className="text-muted">Order value</p>
-                                <h3 className="text-primary fs-30 font-weight-medium">12.3k</h3>
-                                </div>
-                                <div className="mr-5 mt-3">
-                                <p className="text-muted">Orders</p>
-                                <h3 className="text-primary fs-30 font-weight-medium">14k</h3>
-                                </div>
-                                <div className="mr-5 mt-3">
-                                <p className="text-muted">Users</p>
-                                <h3 className="text-primary fs-30 font-weight-medium">71.56%</h3>
-                                </div>
-                                <div className="mt-3">
-                                <p className="text-muted">Downloads</p>
-                                <h3 className="text-primary fs-30 font-weight-medium">34040</h3>
-                                </div> 
-                            </div>
-                            <canvas id="order-chart"></canvas>
-                            </div>
-                        </div>
+                        
+                        <WidgetCal3/>
+                       
                         </div>
                         <div className="col-md-6 grid-margin stretch-card">
                         
