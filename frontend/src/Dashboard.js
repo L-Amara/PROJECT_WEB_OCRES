83c99e07e455de2d5e61b95cab98d7088406e9f7
RemiNem <playeremi764@gmail.com>
2021-12-10 03:05:17 +0100

    logo

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -37,7 +37,7 @@ class Dashboard extends React.Component{
                 
                 <nav className="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
                 <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                    <a className="navbar-brand brand-logo mr-5" href="index.html"><img src="images/logo.svg" className="mr-2" alt="logo"/></a>
+                    <a className="navbar-brand brand-logo mr-5" href="index.html"><img src="images/Aware1.png" className="mr-2" alt="logo"/><img src="images/Aware2.png" className="mr-2" alt="logo"/></a>
                     <a className="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo"/></a>
                 </div>
                 <div className="navbar-menu-wrapper d-flex align-items-center justify-content-end">
